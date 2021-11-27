1e1534543c0e3171c365d522f6fa7f7324760621
TomaszCieslar <tcieslar@gmail.com>
2021-11-27 09:00:58 +0100

    update

---
+++ KursPodstawy/Rozdział2/Lekcja_2_10.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, fillcolor
+import turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.left(90)
+t.color('gold')
+t.fillcolor('red')
+t.pensize(10)
+t.begin_fill()
+t.forward(200)
+t.left(120)
+t.forward(200)
+t.left(120)
+t.forward(200)
+t.end_fill()
+turtle.done()
