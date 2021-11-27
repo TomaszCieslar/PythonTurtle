02f88250881bfbf1009d5741931122f6c41ae303
TomaszCieslar <tcieslar@gmail.com>
2021-11-27 09:15:39 +0100

    update

---
+++ KursPodstawy/Rozdział2/Zadanie domowe/Cwiczenie_2_3.py
@@ -0,0 +1,27 @@
+import turtle
+t = turtle.Turtle()
+t.color('gold')
+t.hideturtle()
+t.pensize(10)
+t.setheading(45)
+
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.forward(-200)
+
+
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+turtle.done()
