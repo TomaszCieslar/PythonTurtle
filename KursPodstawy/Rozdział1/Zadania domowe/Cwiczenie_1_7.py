72be6a1eb4978ba1d875a29166405f18dae826a0
TomaszCieslar <tcieslar@gmail.com>
2021-11-27 08:37:16 +0100

    update

---
+++ KursPodstawy/Rozdział1/Zadania domowe/Cwiczenie_1_7.py
@@ -0,0 +1,22 @@
+import turtle
+t = turtle.Turtle()
+t.pensize(5)
+t.setheading(270)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.left(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+
+
+
+turtle.done()
---
+++ KursPodstawy/Rozdział1/Zadania domowe/Cwiczenie_1_8.py
@@ -0,0 +1,31 @@
+import turtle
+t = turtle.Turtle()
+t.pensize(5)
+t.setheading(270)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.left(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+
+t.left(180)
+t.forward(100)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+
+turtle.done()
