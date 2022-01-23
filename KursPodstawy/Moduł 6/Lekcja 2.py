64f98b2f29094aebae7f31aee07ab26f182ea46e
TomaszCieslar <tcieslar@gmail.com>
2022-01-23 05:03:13 +0100

    update

--- KursPodstawy/Moduł 6/Lekcja 2.py
+++ KursPodstawy/Moduł 6/Lekcja 2.py
@@ -1,5 +1,6 @@
 import turtle
 t = turtle.Turtle()
+
 t.hideturtle()
 t.pensize(5)
 t.color('blue')
@@ -7,6 +8,5 @@ t.color('blue')
 for i in range(6):
     t.forward(100)
     t.right(60)
-
 
 turtle.done()
--- KursPodstawy/Moduł 6/Lekcja 3.py
+++ KursPodstawy/Moduł 6/Lekcja 3.py
@@ -1,12 +1,15 @@
 import turtle
 t = turtle.Turtle()
+
 t.hideturtle()
 t.pensize(5)
 t.color('blue')
+t.speed(1)
 
 for i in range(8):
-    t.forward(100)
+    t.forward(150)
     t.right(45)
+
 
 
 turtle.done()
